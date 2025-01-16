d15a4aa5cf3135d21e638d525e6538b11127f8b5
Clive Thompson <clivethompson09@gmail.com>
2025-01-16 22:15:36 +0000

    commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Navbar from "@/components/Navbar";
+export default function Home() {
+  return (
+    <>
+    <div className="bg-gray-400 h-full w-full">
+    </div>
+    </>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Navbar() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    return (
+        <nav className="bg-gray-800 py-2">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div className="relative flex h-16 items-center justify-between">
+                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                        <button
+                            type="button"
+                            className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white"
+                            aria-controls="mobile-menu"
+                            aria-expanded={mobileMenuOpen}
+                            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            {mobileMenuOpen ? (
+                                <svg className="block size-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            ) : (
+                                <svg className="block size-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                </svg>
+                            )}
+                        </button>
+                    </div>
+                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="flex shrink-0 items-center bold font-semibold rounded-md h-9">
+                            <Image
+                                className="w-auto rounded-full border-2 border-red-500"
+                                src="/companylogo.png"
+                                alt="Your Company"
+                                width={50}
+                                height={50}
+                            />
+                        </div>
+                        <div className="hidden sm:block sm:ml-6">
+                            <div className="flex space-x-4">
+                                <Link href="/" className="text-white bg-gray-900 rounded-md px-3 py-2 text-sm font-semibold">Home</Link>
+                                <Link href="/about" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-semibold">About us</Link>
+                                <Link href="/blog" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-semibold">Blog</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {mobileMenuOpen && (
+                <div className="sm:hidden" id="mobile-menu">
+                    <div className="space-y-1 px-2 pb-3 pt-2">
+                        <Link href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Home</Link>
+                        <Link href="/about" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About Us</Link>
+
+                        <Link href="/blog" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-semibold">Blog</Link>
+                    </div>
+                </div>
+            )}
+        </nav>
+    );
+}
