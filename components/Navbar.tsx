224b32a26ec54a53e319aeb480e27f3c2bbc0732
Clive Thompson <clivethompson09@gmail.com>
2025-01-16 22:26:42 +0000

    commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
     return (
-        <nav className="bg-gray-800 py-2">
+        <nav className="bg-white border-2 border-x-0 border-t-0 border-black shadow py-2">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
