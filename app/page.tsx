4749c7cb7312a64e8843cba28084ae5887e698fe
CT <138801309+CT62@users.noreply.github.com>
2025-04-07 22:31:42 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -82,7 +82,7 @@ const HomePage = () => {
           </div>
           <div className="flex items-center gap-2">
             <Mail className="w-5 h-5" />
-            <span>jack@jfhomeimprovementss.ie</span>
+            <span>jack@jfhomeimprovements.ltd</span>
           </div>
           <div className="flex items-center gap-2">
             <Clock className="w-5 h-5" />
